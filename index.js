f698d4cd448f1e86fd4e3ba00e0f8e9acb68252e
Josh Cesana <joshuacesana@gmail.com>
2018-05-07 06:55:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 function theBeatlesPlay(musicians, instruments) {
   var output = [];
   
+  for (let i = 0; i < musicians.length; i++) {
+    var phrase = `${musicians[i]} plays ${instruments[i]}`;
+    output.push(phrase);
+  }
   return output;
 }
